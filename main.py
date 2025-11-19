ce1987f3e575a7eeb637dd3c97cd74207013f3b0
Melodie X <mxiong@uwaterloo.ca>
2025-11-18 21:11:03 -0500

    attempting to find new model

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import pyautogui as pag
 model = YOLO('runs/detect/train10/weights/best.pt')
 
 class_names = model.names
+screen_size = pag.size()
+screen_Centerx = screen_size.width / 2
+screen_Centery = screen_size.height / 2
 
 def main():
   cap = cv2.VideoCapture(0)
@@ -24,20 +27,21 @@ def main():
       class_name = class_names[id]
       detected_classes.add(class_name)
 
-    if 'thumb_index' in detected_classes:
-      pag.leftClick(None, None)
+    # if 'thumb_index' in detected_classes:
+    #   pag.leftClick(None, None)
 
-    if 'thumb_middle' in detected_classes:
-      pag.rightClick(None, None)
+    # if 'thumb_middle' in detected_classes:
+    #   pag.rightClick(None, None)
 
-    if 'thumb_out' in detected_classes:
-      pag.scroll(50) 
+    # if 'thumb_out' in detected_classes:
+    #   pag.scroll(50) 
 
-    if 'pinky_out' in detected_classes:
-      pag.scroll(-50)
+    # if 'pinky_out' in detected_classes:
+    #   pag.scroll(-50)
 
     if 'fist' in detected_classes:
-      pag.moveTo(x_center, y_center)    
+      #Treat the middle of the video as the center of screen
+      pag.moveTo(screen_Centerx + (x_center * 3), screen_Centery + (y_center * 3))    
 
     annotated_frame = result[0].plot()
     cv2.imshow('handcursorx', annotated_frame)
